2c657501a39329434ef9c0d45ad911b141b6a9f1
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-13 15:45:41 +0800

    Add user_id to Articles schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_072352) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_074016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
