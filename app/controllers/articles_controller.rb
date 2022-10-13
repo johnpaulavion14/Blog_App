36d2a45f1bc9018caca78c0940aa523cf5c24cd4
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-13 16:50:43 +0800

    Render edit page after update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
+        format.html { render :edit, notice: "Article was successfully updated." }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
