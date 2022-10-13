32776c8cfb5f99f6b8731c929eb0f477a53a550b
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-13 16:47:00 +0800

    Add delete article in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
+  delete "articles/:id" => 'articles#destroy', as: "delete_article"
+
   devise_for :users
   get 'dashboard/index'
   root 'home#index'
