4ece7e383e96a0151b4032f8ea1f0007d7eb1171
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-13 16:10:53 +0800

    Add body in Article schema, modify views and controller

--- db/migrate/20221013080506_add_fields_to_articles.rb
+++ db/migrate/20221013080506_add_fields_to_articles.rb
@@ -1,6 +1,5 @@
 class AddFieldsToArticles < ActiveRecord::Migration[7.0]
   def change
-    add_column :articles, :user_id, :integer
-    add_index :articles, :user_id
+    add_column :articles, :body, :text
   end
 end
