cf2b3a2d58d1bb372888d03b67a0f02da923d3af
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-19 16:17:44 +0800

    Update schema

--- db/migrate/20221013073527_create_articles.rb
+++ db/migrate/20221013073527_create_articles.rb
@@ -3,8 +3,11 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     create_table :articles do |t|
       t.string :title
       t.string :description
+      t.text :body
+      t.integer :user_id
 
       t.timestamps
     end
+    add_index :articles, :user_id
   end
 end
--- db/migrate/20221013080506_add_fields_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldsToArticles < ActiveRecord::Migration[7.0]
-  def change
-    add_column :articles, :body, :text
-  end
-end
