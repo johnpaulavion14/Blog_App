a3b8329ffff1d776231b176204f6fee20f34ffae
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-20 12:24:06 +0800

    Fix path after registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :articles
   delete "articles/:id" => 'articles#destroy', as: "delete_article"
 
-  devise_for :users
   get 'dashboard/index'
   root 'home#index'
 
