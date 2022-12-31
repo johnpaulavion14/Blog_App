beac72175956c72bc694606adb0fe39e786f610b
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-12-31 13:45:18 +0800

    test push

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_080506) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_073527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.text "body"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
